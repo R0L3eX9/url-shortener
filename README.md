b468088603c571c5be232a8705a056210d080df6
Razvan Maracine <razvan9rolex@outlook.com>
2023-05-30 21:54:01 +0300

    Redis integration v1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Go Short
+A URL Shortener written in Go to learn more about Go and Redis.
+
+
+## Endpoints
+POST - /api/create -> requires the url to be shorten
+GET - /app/<shorten_url> -> redirects user to the url linked to the shorten_url
+
+
+## TODO
+* [ ] Run Redis on localhost
+* [ ] Successfully set a value in the Redis db
+* [ ] Successfully read a value from the Redis db
+* [ ] Successfully redirect the user to the desired address 
--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"context"
+    "encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+    "strings"
+
+	"github.com/redis/go-redis/v9"
 )
 
-type ShortenURL struct {
-	OriginalUrl string
-	Url         string
+type URL struct {
+	url         string
 }
 
 func generate_url() string {
+    // TODO: change rand seed
 	const CHAR_SET = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	const URL_LEN = 10
 
@@ -36,9 +41,21 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := generate_url()
-	fmt.Fprintf(w, "Your url is: %s\n", url)
-	// generate a random url
+
+    var url string
+    json.NewDecoder(r.Body).Decode(&url)
+	shorten_url := generate_url()
+
+    ctx := context.Background()
+    client := connect_db()
+    err := client.Set(ctx, shorten_url, url , 0).Err()
+
+    if err != nil {
+        http.Error(w, "Database is down", http.StatusExpectationFailed)
+    }
+
+
+	fmt.Fprintf(w, "Your url is: %s\n", shorten_url)
 	// make sure it has not been used
 	// insert into db
 }
@@ -48,17 +65,32 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not supported", http.StatusNotFound)
 		return
 	}
-	fmt.Println(w, "TODO")
+    client := connect_db()
+    id := strings.TrimPrefix(r.URL.Path, "/app/")
+    ctx := context.Background()
+    shorten_url, err := client.Get(ctx, id).Result()
+    if err != nil {
+        http.Error(w, "URL not found in the database", http.StatusBadRequest)
+    }
+    http.Redirect(w, r, shorten_url, http.StatusSeeOther)
 	// check if the url is in db
 	// redirect if it is
 }
 
+func connect_db() *redis.Client {
+    return redis.NewClient(&redis.Options{
+        Addr: "localhost:8888",
+        Password: "",
+        DB: 0,
+    })
+}
+
 func main() {
 	// POST - /api/create -> give a url and we generate a random url
-	// GET  - /api/<shorten-url> -> sends user to the actual url
+	// GET  - /app/<shorten-url> -> sends user to the actual url
 
 	http.HandleFunc("/api/create", createHandler)
-	http.HandleFunc("/app/", redirectHandler)
+    http.HandleFunc("/app/", redirectHandler)
 
     fmt.Println("Serving on http://localhost:8080")
     if err := http.ListenAndServe(":8080", nil); err != nil {
